c4ba3c04316f7af1a9931824ead59cb893ea2d44
VanDung <3ngk1sha@gmail.com>
2025-05-08 14:12:08 +0700

    test

--- api/src/user/seeds/model.seed.ts
+++ api/src/user/seeds/model.seed.ts
@@ -6,10 +6,8 @@
  * 2. All derivative works must include clear attribution to the original creator and software, Hexastack and Hexabot, in a prominent location (e.g., in the software's "About" section, documentation, and README file).
  */
 
-import { Injectable } from '@nestjs/common';
-
 import { BaseSeeder } from '@/utils/generics/base-seeder';
-
+import { Injectable } from '@nestjs/common';
 import { ModelRepository } from '../repositories/model.repository';
 import { Model, ModelFull, ModelPopulate } from '../schemas/model.schema';
 
